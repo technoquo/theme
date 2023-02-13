47e11dd76e255cb1d0832ff1b40781b5dde64fe3
Leonel <technoquo@gmail.com>
2023-02-13 10:58:29 +0100

    credenciales test

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'plantilladb' );
+define( 'DB_NAME', 'u763605788_platilla' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u763605788_lelobo' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'pass 5V2chQEwT8*' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
