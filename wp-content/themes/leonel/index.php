1430db0a56e90ca06b7a0e9cb5033b29141a128b
Leonel <technoquo@gmail.com>
2023-02-13 14:13:00 +0100

    tema personalizado

---
+++ wp-content/themes/leonel/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <title>Blog Template for Bootstrap</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">    
+  </head>
+  <body>
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="#">Inicio</a>
+          <a class="blog-nav-item" href="#">Página 1</a>
+          <a class="blog-nav-item" href="#">Página 2</a>
+          <a class="blog-nav-item" href="#">Página 3</a>
+          <a class="blog-nav-item" href="#">Contacto</a>
+        </nav>
+      </div>
+    </div>
+
+    <section class="showcase">
+      <div class="container">
+        <h1>Plantilla Wordpress rende2web</h1>
+        <p>pum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to m</p>
+        <a class="btn btn-primary btn-lg">Ver más</a>
+      </div>
+    </section>
+
+    <section class="cajas">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="caja">
+             <img src="img/beautiful-girl-2003647_1920-2.jpg" class="img-responsive">
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="caja">              
+              <img src="img/fashion-3080644_1920-2.jpg" class="img-responsive">
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="caja">              
+               <img src="img/girl-1756727_1920-2.jpg" class="img-responsive">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="contenido">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+             <h2>Página de ejemplo</h2>
+             <p>Esta es una página de ejemplo, Es diferente a una entrada de un blog porque se mantiene estática y se mostrará en la barra de navegación (en la mayoría de temas). Casi todo el mundo comienza con una página Sobre mí para presentarse a los potenciales visitantes. Podría ser algo así:</p>
+             <p><iframe width="560" height="315" src="https://www.youtube.com/embed/D2EvaSgi3UQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></p>
+          </div>
+
+          <div class="col-md-4 modulo-sidebar">
+             <h4>Calendario</h4>
+             <img src="img/Calendario.jpg" class="img-responsive">
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+
+    <footer class="blog-footer">
+      <p>© 2018 - Megacurso de Wordpress</p>
+      <p>
+        <a href="#">Volver arriba</a>
+      </p>
+    </footer>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
